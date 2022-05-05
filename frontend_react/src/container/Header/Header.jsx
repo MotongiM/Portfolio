c14f0846556b57b93e3829d3455bd199011677a1
Michel <Michel_motongi@hotmail.com>
2022-05-05 14:32:48 -0400

    Change the header

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -16,22 +16,22 @@ const scaleVariants= {
 
 const Header = () => {
   return (
-    <div className="app_header app__flex ">
+    <div id="home" className="app__header app__flex ">
       <motion.div
         whileInView= {{ x: [-100, 0], opacity: [0, 1] }} 
         transition= {{ duration: 0.5 }} 
-        className="app_header-info"
+        className="app__header-info"
       >
         <div className="app__header-badge">
           <div className="badge-cmp app__flex">
             <span>👋</span>
             <div style={{ marginLeft: 20}}>
               <p className='p-text'>Hello, I am</p>
-              <h1 className='head-text'>Michel</h1>  
+              <h1 className='head-text'>Michel </h1>  
             </div>
           </div>
           <div className="tag-cmp app__flex">
-            <p className='p-text'>Web Developper</p>
+            <p className='p-text'>Web Developer</p>
             <p className='p-text'>Freelancer</p>
           </div>  
         </div>
