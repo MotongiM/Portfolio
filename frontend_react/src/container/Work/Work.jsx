0a2f6ee72b1db834eeb8cecc39f4850f97e0c5c7
Michel <Michel_motongi@hotmail.com>
2022-06-09 15:47:05 -0400

    Finish working on the logic to implement future work project

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -7,11 +7,11 @@ import './Work.scss';
 
 const Work = () => {
   const [activeFilter, setactiveFilter] = useState('All');
-  const [animateCard, setanimateCard] = useState({y: 0, opacity: 1});
+  const [animateCard, setanimateCard] = useState({ y: 0, opacity: 1});
   const [works, setWorks] = useState([])
   const [filterWork, setFilterWork] = useState([]);
   useEffect(() => {
-    const query = '*[_type == "work]';
+    const query = '*[_type == "works"]';
     
     client.fetch(query)
       .then((data) => {
@@ -20,7 +20,18 @@ const Work = () => {
       })
   })
   const handleWorkFilter = (item) => {
+    setactiveFilter(item);
+    setanimateCard([{y:100, opacity: 0 }]);
+    
+    setTimeout(() => {
+      setanimateCard([{y:0, opacity: 1 }]);
 
+      if(item === 'All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)))
+      }
+    }, 500)
   }
   return (
     <>
@@ -36,27 +47,62 @@ const Work = () => {
             onClick={() => handleWorkFilter(item)}
             className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
           >
-            <motion.div 
-              animate={animateCard} 
-              transition={{ duration: 0.5, delayChildren: 0.5 }} 
-              className="app__work-portfolio"
-            >
-              {filterWork.map((work, index) => (
-                <div className='app__work-item app__flex' key={index}>
-                  <div className='app__work-img app__flex'>
-                    <img src={urlFor(work.imgUrl)} alt={work.name} />
-
-                  </div>
-
-                </div>
-              ))}
-            </motion.div>
             {item}
           </div>
         ))}
-      </div>
+      </div>  
+      <motion.div 
+        animate={animateCard} 
+        transition={{ duration: 0.5, delayChildren: 0.5 }} 
+        className="app__work-portfolio"
+      >
+        {filterWork.map((work, index) => (
+          <div className='app__work-item app__flex' key={index}>
+            <div className='app__work-img app__flex'>
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
+              
+              <motion.div
+                whileHover={{opacity: [0, 1]}}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
+                className="app__work-hover app__flex"
+              >
+                <a href={work.projectLink} target=" blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1]}}
+                    whileHover={{opacity: [1, 0.9]}}
+                    transition={{ duration: 0.25}}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div> 
+
+                </a>
+                <a href={work.codeLink} target=" blank" rel="norefer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1]}}
+                    whileHover={{opacity: [1, 0.9]}}
+                    transition={{ duration: 0.25}}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div> 
+
+                </a>
+
+              </motion.div>
+            </div>
+            <div className='app__work-content app__flex'>
+              <h4 className='bold-text'>{work.title}</h4>
+              <p className='p-text' style={{ marginTop: 10}}>{work.description}</p>
+              <div className="app__work-tag app__flex">
+                <p className='p-text'>{work.tags[0]}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </motion.div>
     </>
   )
 }
 
-export default Work
+export default AppWrap(Work, 'work')
