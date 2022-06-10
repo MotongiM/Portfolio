ad55aa4493cda79a9e4e92dc4423fd98bf213906
Michel <Michel_motongi@hotmail.com>
2022-06-10 14:59:34 -0400

    Change logo/profile

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -17,7 +17,7 @@ import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
-
+import logo from '../assets/logo.png';
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
@@ -25,7 +25,6 @@ import about04 from '../assets/about04.png';
 
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
-import logo from '../assets/logo.png';
 
 import adidas from '../assets/adidas.png';
 import amazon from '../assets/amazon.png';
