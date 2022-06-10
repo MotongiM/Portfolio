b7fabc7bfa1f2dc5e529f953bb89b4cf1270eb43
Michel <Michel_motongi@hotmail.com>
2022-06-10 15:03:43 -0400

    Add motionwrap

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { AiFillEye, AiFillGithub} from 'react-icons/ai'
 import { motion } from 'framer-motion';
-import { AppWrap } from '../../wrapper/';
+import { AppWrap , MotionWrap} from '../../wrapper/';
 import { urlFor, client } from '../../client';
 import './Work.scss';
 
@@ -18,7 +18,7 @@ const Work = () => {
         setWorks(data);
         setFilterWork(data);
       })
-  })
+  }, [])
   const handleWorkFilter = (item) => {
     setactiveFilter(item);
     setanimateCard([{y:100, opacity: 0 }]);
@@ -105,4 +105,8 @@ const Work = () => {
   )
 }
 
-export default AppWrap(Work, 'work')
+export default AppWrap(
+  MotionWrap(Work, 'app__works'), 
+  'work',
+  'app__primarybg'
+);
